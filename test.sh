4371920000fec62ecac639b27e8c422f10aa60b1
korya <korya.github@gmail.com>
2018-08-01 23:13:58 -0400

    Update test

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-docker run --rm ${IMAGE_NAME} locale | grep LANG=en_US.UTF-8
-docker run --rm ${IMAGE_NAME} locale | grep LC_ALL=en_US.UTF-8
+docker run --rm ${IMAGE_NAME} locale | grep -q LANG=en_US.UTF-8
+docker run --rm ${IMAGE_NAME} locale | grep -q LC_ALL=en_US.UTF-8
 
 docker run --rm ${IMAGE_NAME} bash -c '\
   useradd dummy_user; \
-  su dummy_user -c "gosu root ls -la /root"; \
-'
+  su dummy_user -c "gosu root ls -a /root"; \
+' | grep -q '^.bashrc$'
 
 docker run --rm ${IMAGE_NAME} tini --version | \
   grep -q '^tini version 0.18.0 - git.fec3683$'
